560d69392fa2eb3f7283050a0f5aab56f3712680
GaryQuan <garylyq17@gmail.com>
2020-06-01 17:40:01 +0800

    Frontend mentor challenge solution

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'karla': ["'Karla'", 'sans']
+      },
+      colors: {
+        'cyan': 'hsl(179, 62%, 43%)',
+        'bright-yellow': 'hsl(71, 73%, 54%)'
+      },
+      height: {
+        '40-vh': '40vh',
+        '60-vh': '60vh'
+      }
+    }
+  },
+  variants: {},
+  plugins: [],
+}
